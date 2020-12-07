fd5f78f622bcdb5e7a66f24a1ced5f6c3ecc84e9
yumoz <zealouszxl@163.com>
2020-12-07 22:33:14 +0800

    add reverse list

--- TestData/List/sList.c
+++ TestData/List/sList.c
@@ -70,8 +70,132 @@ void SListPrint(SListNode*phead)
 {
 	SListNode* cur = phead;
 	while (cur!=NULL){
-		printf("%d->",cur->data);
+		printf("	%4d ",cur->data);
 		cur = cur->next;//前一个结构体成员next存放下一个结构体指针
 	}
-	printf("NULL\n");
+	printf("    NULL\n");
+}
+
+//头插
+void SListPushFront(SListNode** pplist, SListDataType x)
+{
+	assert(pplist);
+
+	// 1.空
+	// 2.非空
+	SListNode* newnode = BuySListNode(x);
+	if (*pplist == NULL)
+	{
+		*pplist = newnode;
+	}
+	else
+	{
+		newnode->next = *pplist;
+		*pplist = newnode;
+	}
+}
+
+//头删
+//void SListPopFront(SListNode** pplist)
+//{
+//	// 1.空
+//	// 2.一个
+//	// 3.两个及以上
+//	SListNode* first = *pplist;
+//	if (first == NULL)//空
+//	{
+//		return;
+//	}
+//	else if (first->next == NULL)//一个节点
+//	{
+//		free(first);
+//		*pplist = NULL;
+//	}
+//	else
+//	{
+//		SListNode* next = first->next;
+//		free(first);
+//		*pplist = next;
+//	}
+//}
+//头删方法2
+//需要改变头部指针
+void SListPopFront(SListNode** pplist)
+{
+	// 1.空
+	// 2.一个 + 3.两个及以上
+	SListNode* first = *pplist;
+	if (first == NULL)//空
+	{
+		return;
+	}
+	else 
+	{
+		SListNode * next = first->next;
+
+		free(first);
+		*pplist = next;
+	}
+}
+//查找，并修改
+SListNode* SListFind(SListNode* plist, SListDataType x)
+{
+	SListNode* cur = plist;
+	while (cur)
+	{
+		if (cur->data == x)
+			return cur;
+
+		cur = cur->next;
+	}
+
+	return NULL;
+}
+
+//当前位置之后插入，之前插入可以使用双向链表，
+//void SListInsertAfter(SListNode* pos, SListDataType x)
+//{
+//	assert(pos);
+//	SListNode* next = pos->next;//先保存next的地址
+//
+//	// pos newnode next
+//	SListNode* newnode = BuySListNode(x);
+//	pos->next = newnode;
+//	newnode->next = next;
+//}
+
+//单链表有缺点，哈希图和单链表
+void SListInsertAfter(SListNode* pos, SListDataType x)
+{
+	assert(pos);
+	
+	SListNode* newnode = BuySListNode(x);
+	newnode->next = pos->next;
+	pos->next = newnode;
+
+}
+//删除后一位置
+//void SListEraseAfter(SListNode* pos)
+//{
+//	assert(pos);
+//	// pos next nextnext
+//	SListNode* next = pos->next;
+//
+//	if (next != NULL)
+//	{
+//		SListNode* nextnext = next->next;
+//		free(next);
+//		pos->next = nextnext;
+//	}
+//}
+void SListEraseAfter(SListNode* pos)
+{
+	assert(pos);
+	if (pos->next)
+	{
+		SListNode* next = pos->next;
+		SListNode* nextnext = next->next;
+		pos->next = nextnext;
+		free(next);
+	}
 }
--- TestData/List/test.c
+++ TestData/List/test.c
@@ -1,24 +1,49 @@
 #include "SList.h"
 
-int main()
+void testSList()
 {
 	//SListNode* phead = NULL;
-	SListNode * pList=NULL;
-		
+	SListNode * pList = NULL;
+	printf("Î²²å£º\n");
 	SListPushBack(&pList, 1);
 	SListPushBack(&pList, 2);
 	SListPushBack(&pList, 3);
 	SListPushBack(&pList, 4);
-
 	SListPrint(pList);
 
-	SListPopBack(&pList);
-	SListPopBack(&pList);
-	SListPopBack(&pList);
-	SListPopBack(&pList);
-
+	printf("Í·²å£º\n");
+	SListPushFront(&pList, -1);
+	SListPushFront(&pList, -2);
+	SListPushFront(&pList, -3);
 	SListPrint(pList);
 
+	printf("Î²É¾£º\n");
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPrint(pList);
+
+	printf("Í·É¾£º\n");
+	SListPopFront(&pList);
+	SListPopFront(&pList);
+	SListPrint(pList);
+
+	printf("Find and change:\n");
+	SListNode * pos =SListFind(pList, 2);
+	if (pos){
+		pos->data = 30;
+	}
+	SListPrint(pList);
+
+	//printf("ÖÐ²å£º\n");
+	//SListInsertAfter(&pList,1);
+
+	//SListDestory(pList);
+	//SListPrint(pList);
+
+}
+int main()
+{
+	testSList();
 	system("pause");
 	return 0;
 }
